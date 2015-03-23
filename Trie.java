a682f215d3cc588cb262de7be3ad77d7d3615da1
Shirley Young <yutian.yang.88@gmail.com>
2015-03-22 23:18:54 -0400

    LargestRectangleOfWords
    Given a dictionary of millions of words, give an algorithm to find the
    largest possible rectangle of letters such that every row forms a word
    (reading left to right) and every column forms a word (reading top to
    bottom).

---
+++ Trie.java
@@ -0,0 +1,46 @@
+package RectangleWords;
+import java.util.*;
+public class Trie {
+	protected final Map<Character, Trie> children;
+	protected String value;
+	protected boolean isWord = false;
+	private Trie(String value){
+		this.value = value;
+		children = new HashMap<Character, Trie>();
+	}
+	public Trie(){
+		this("");
+	}
+	
+	public void add(String word){
+		if(word == null || word.length() == 0){
+			System.out.println("Cannot add null or empty string!");
+			return;
+		}
+		Trie node = this;
+		for(char c : word.toCharArray()){
+			if(!node.children.containsKey(c)){
+				node.put(c);
+			}
+			node = node.children.get(c);
+		}
+		isWord = true;
+	}
+	private void put(char c){
+		children.put(c, new Trie(this.value + c));
+	}
+	public void add(Collection<String> dict){
+		for(String word : dict)
+			add(word);
+	}
+	public boolean contains(String s){
+		Trie node = this;
+		for(char c : s.toCharArray()){
+			if(!node.children.containsKey(c))
+				return false;
+			node = node.children.get(c);
+		}
+		return node.value.equals(s);
+	}
+	
+}
